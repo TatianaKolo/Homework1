8b6b4ec73722e9a2aebf699d9bf602fe88e2154b
TatianaKolo <manila-nick@yandex.ru>
2022-11-22 17:15:46 +0300

    finished task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,5 +1,5 @@
-﻿int a = -9;
-int b = -3;
+﻿int a = 2;
+int b = 10;
 int max = a;
 int min = b;
 
@@ -7,5 +7,7 @@ if (a > max) max = a;
 if (b > max) max = b;
 if (a < min) min = a;
 if (b < min) min = b;
+Console.Write ("min = ");
 Console.WriteLine (min);
+Console.Write ("max = ");
 Console.WriteLine (max);
