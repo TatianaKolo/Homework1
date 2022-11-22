a18ce68d6ca45651522a6fc76894cb0891758239
TatianaKolo <manila-nick@yandex.ru>
2022-11-22 18:12:29 +0300

    done first variant of task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 2;
+int b = 3;
+int c = 7;
+int max = a;
+int min = b;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write ("max = ");
+Console.WriteLine (max);
