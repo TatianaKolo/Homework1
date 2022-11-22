dc4ba8a4e4d4ab4859a31cb085a0cf1cdc73d20c
TatianaKolo <manila-nick@yandex.ru>
2022-11-22 15:42:13 +0300

    done 2nd varint of task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,7 +1,11 @@
 ﻿int a = 2;
 int b = 10;
 int max = a;
+int min = b;
 
 if (a > max) max = a;
 if (b > max) max = b;
+if (a < min) min = a;
+if (b < min) min = b;
+Console.WriteLine (min);
 Console.WriteLine (max);
